2af29034d9b289e598bbf8fe4c7b8622beca5b1b
HP <HP@AMD>
2023-03-20 19:34:13 +0530

    First commit

--- src/TraningCodes/Test2.java
+++ src/TraningCodes/Test2.java
@@ -1,8 +1,30 @@
 package TraningCodes;
 
+
+
+import java.util.Scanner;
 public class Test2 {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the age");
+		int age=sc.nextInt();
+		
+		if(age < 18 ) {
+			System.out.println("you are still minor");
+		}
+		else if(age > 65) {
+			System.out.println("you are senior citizen");
+		}
+		else {
+			System.out.println("Congragulations");
+		}
+	
+		
+		
 		// TODO Auto-generated method stub
 
 	}
