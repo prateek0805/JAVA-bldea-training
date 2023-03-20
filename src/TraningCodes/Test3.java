4005caaf12e9fed928e4cb026fb5b81b72d6f38b
HP <HP@AMD>
2023-03-20 19:36:46 +0530

    First commit

--- src/TraningCodes/Test3.java
+++ src/TraningCodes/Test3.java
@@ -1,10 +1,34 @@
 package TraningCodes;
 
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author HP
+ *
+ */
 public class Test3 {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n =sc.nextInt();
+				for(int i=1;i<=n;i++) {
+					for(int j=1;j<=i;j++) {
+						System.out.print(" ");
+					}for(int j=1;j<=n;j++) {
+						System.out.print("* ");
+					}
+					System.out.println();
+				}
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
 
 	}
 
